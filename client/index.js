b74dd100a751a83a900592785e8ea8503e1c7ff7
edgrams <edgramswork@gmail.com>
2017-10-20 23:02:05 -0500

    working react example

--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-console.log('this is just a test.');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
