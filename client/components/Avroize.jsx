b0ef36b151726097888111e7371661fa1efaf626
edgrams <edgramswork@gmail.com>
2017-10-21 16:05:28 -0500

    avsc + validation

--- client/components/Avroize.jsx
+++ client/components/Avroize.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "../styles/Avroize.css"
+import greenCheckBox from "../images/green-checkbox.png";
+import redCheckBox from "../images/red-checkbox.png";
+
+import avsc from "avsc";
 
 export default class Avroize extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            validSchema: null
+        };
+
+        this.handleSchemaChange = this.handleSchemaChange.bind(this);
+    }
+
+    handleSchemaChange(event) {
+        const schema = event.target.value;
+        try {
+            avsc.parse(schema);
+            this.setState({
+                validSchema: true
+            });
+
+        } catch (e) {
+            this.setState({
+                validSchema: false
+            });
+        }
+    }
+
     render() {
         return (
             <div className="avroize">
@@ -9,12 +37,13 @@ export default class Avroize extends React.Component {
 
                 <div>
                     <h3>Avro schema</h3>
-                    <textarea placeholder="<< insert schema here >>" />
+                    <textarea placeholder="<< insert schema here >>" onChange={this.handleSchemaChange} />
+                    { this.state.validSchema ? <img src={greenCheckBox} /> : <img src={redCheckBox} /> }
                 </div>
 
                 <div>
                     <h3>Data</h3>
-                    <textarea placeholder="<< insert data here >>" />
+                    <textarea placeholder="<< insert schema here >>" />
                 </div>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.(png|jpg)$/, loader: 'url-loader', options: { limit: 25000 } },
             { test: /\.js$/, loader: "babel-loader" },
             { test: /\.jsx$/, loader: "babel-loader" }
         ]
