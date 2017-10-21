00df2dfe88efbe2d6f1a3f71a5a91d6697c48438
edgrams <edgramswork@gmail.com>
2017-10-21 16:30:48 -0500

    JSON validation

--- client/components/Avroize.jsx
+++ client/components/Avroize.jsx
@@ -9,28 +9,70 @@ export default class Avroize extends React.Component {
     constructor() {
         super();
         this.state = {
+            validJSON: null,
             validSchema: null
         };
 
+        this.handleDataChange = this.handleDataChange.bind(this);
         this.handleSchemaChange = this.handleSchemaChange.bind(this);
+    }
+
+    handleDataChange(event) {
+        const data = event.target.value;
+
+        if (data.length === 0) {
+            this.setState({
+                validJSON: null
+            });
+        } else {
+            try {
+                const value = JSON.parse(data);
+                console.log(value);
+                this.setState({
+                    validJSON: true
+                });
+
+            } catch (e) {
+                this.setState({
+                    validJSON: false
+                });
+            }
+        }
     }
 
     handleSchemaChange(event) {
         const schema = event.target.value;
-        try {
-            avsc.parse(schema);
-            this.setState({
-                validSchema: true
-            });
 
-        } catch (e) {
+        if (schema.length === 0) {
             this.setState({
-                validSchema: false
+                validSchema: null
             });
+        } else {
+            try {
+                avsc.parse(schema);
+                this.setState({
+                    validSchema: true
+                });
+
+            } catch (e) {
+                this.setState({
+                    validSchema: false
+                });
+            }
         }
     }
 
     render() {
+        let jsonImage = null;
+        if (this.state.validJSON !== null) {
+            jsonImage = this.state.validJSON ? <img src={greenCheckBox} /> : <img src={redCheckBox}/>;
+        }
+
+        let schemaImage = null;
+        if (this.state.validSchema !== null) {
+            schemaImage = this.state.validSchema ? <img src={greenCheckBox} /> : <img src={redCheckBox}/>;
+        }
+
         return (
             <div className="avroize">
                 <h1 className="title">Avroize</h1>
@@ -38,12 +80,13 @@ export default class Avroize extends React.Component {
                 <div>
                     <h3>Avro schema</h3>
                     <textarea placeholder="<< insert schema here >>" onChange={this.handleSchemaChange} />
-                    { this.state.validSchema ? <img src={greenCheckBox} /> : <img src={redCheckBox} /> }
+                    {schemaImage}
                 </div>
 
                 <div>
                     <h3>Data</h3>
-                    <textarea placeholder="<< insert schema here >>" />
+                    <textarea placeholder="<< insert data here >>" onChange={this.handleDataChange} />
+                    {jsonImage}
                 </div>
             </div>
         );
